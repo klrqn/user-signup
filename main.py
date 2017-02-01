50afec1c1a2e41fa561f8b2bc594b6ff15de2a9b
john doe <scottbruceburton@gmail.com>
2017-02-01 12:23:28 -0500

    finished lesson2. Todo: migrate to user-signup

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 import webapp2
 import validation
+import cgi
 
 form="""
 <form method="post">
     What is your birthday?
     <br>
+
     <label>Month<input type="text" name="month" value={1}></label>
     <br>
     <label>Day<input type="text" name="day" value={2}></label>
@@ -16,19 +18,25 @@ form="""
 </form>
 """
 
+def escape_html(s):
+    return cgi.escape(s, quote=True)
+
 class MainHandler(webapp2.RequestHandler):
     def writeform(self, error="", month="", day="", year=""):
-        self.response.write(form.format(error, month, day, year))
+        self.response.write(form.format(error,
+                                        escape_html(month),
+                                        escape_html(day),
+                                        escape_html(year)))
 
     def get(self):
         self.writeform()
 
     def post(self):
-        # verify the user's input
         user_month = self.request.get('month')
         user_day = self.request.get('day')
         user_year = self.request.get('year')
 
+        # verify the user's input
         month = validation.valid_month(user_month)
         day = validation.valid_day(user_day)
         year = validation.valid_year(user_year)
@@ -37,10 +45,13 @@ class MainHandler(webapp2.RequestHandler):
         if not (month and day and year):
             self.writeform("<p style=color:red;> That doesn't look valid to me friend.</p>", user_month, user_day, user_year)
         else:
-            self.response.write("Thanks, that data is totally valid!")
+            self.redirect("/thanks")
 
-        #TODO: include error message
+class ThanksHandler(webapp2.RequestHandler):
+    def get(self):
+        self.response.write("Thanks, that data is totally valid!")
 
 app = webapp2.WSGIApplication([
-    ('/', MainHandler)
+    ('/', MainHandler),
+    ('/thanks', ThanksHandler)
 ], debug=True)
